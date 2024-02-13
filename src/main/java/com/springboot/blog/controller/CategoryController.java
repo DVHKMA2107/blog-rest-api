5d1e13222dd23a3df13d831c99ce41d90a420506
DVHKMA2107 <hoadamviet@gmail.com>
2024-02-13 10:37:54 +0700

    category_crud

---
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -0,0 +1,56 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDTO;
+import com.springboot.blog.service.CategoryService;
+import lombok.Getter;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/categories")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDTO> addCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable("id") Long categoryId) {
+        CategoryDTO categoryDTO = categoryService.getCategory(categoryId);
+        return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategory() {
+        List<CategoryDTO> categories = categoryService.getAllCategory();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDTO> updateCategory(@RequestBody CategoryDTO categoryDTO, @PathVariable(name = "id") Long categoryId) {
+        CategoryDTO category = categoryService.updateCategory(categoryDTO, categoryId);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> deleteCategory(@PathVariable(name = "id") Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>("Category deleted successfully", HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -62,5 +64,11 @@ public class PostController {
     ) {
         postService.deleteById(postId);
         return new ResponseEntity<>(String.format("Delete Post with Id %s success", postId), HttpStatus.OK);
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<List<PostDTO>> getPostsByCategory(@PathVariable(name = "id") Long categoryId) {
+        List<PostDTO> postDTOs = postService.getPostsByCategory(categoryId);
+        return new ResponseEntity<>(postDTOs, HttpStatus.OK);
     }
 }
